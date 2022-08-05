bbc1a8fdd1380b1451bc2f34aaa22be466616d3f
nanaaefua <equansah1@gmail.com>
2022-08-04 19:04:30 -0500

    flask

--- creatingwebsite/Netflex.py
+++ creatingwebsite/Netflex.py
@@ -1,29 +1,24 @@
 # Import Splinter and BeautifulSoup
 import csv
 from json import load
+from matplotlib.pyplot import title
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 
-app = Flask(__name__)
-
-
+#app = Flask(__name__)
 
 # Dependencies and Setup
+
 import pandas as pd
+
 # Import csv
-Netflex_to_load = ("Bootcampgithub", "cleaned_credits.csv")
-Netflex_to_load = ("Bootcampgithub", "titles.csv")
-Netflex_df = pd.read.csv("cleaned_credits.csv")
-df = pd.read.csv
+credit_df = pd.read_csv("Resources/credits.csv")
+title_df= pd.read_csv("Resources/titles.csv")
+credit_df.to_html("credits.html")
+title_df.to_html("title.html")
 
 
-# Set up Splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-
-html = browser.html
-news_soup = soup(html, 'html.parser')
 
 
 
